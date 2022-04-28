438ddedfbaf74916655f5de8ecfdc1f9fb6465eb
Hasan <artykovh@gmail.com>
2022-04-28 22:14:31 +0600

    add register and login for user

--- backend/apps/product/urls.py
+++ backend/apps/product/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-      path('getSubcategory/', get_subcategory, name = 'get_subcategory')
+      path('getSubcategory/', get_subcategory, name = 'get_subcategory'),
+      path('', IndexPage.as_view(), name='index'),
   ]
 
 
--- backend/apps/product/views.py
+++ backend/apps/product/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 from .models import SubCategory
+from django.views.generic import TemplateView
 
 # Create your views here.
 def get_subcategory(request):
@@ -10,3 +11,7 @@ def get_subcategory(request):
     result = list(SubCategory.objects.filter(
         category_id=int(id)).values('id', 'name'))
     return HttpResponse(json.dumps(result), content_type="application/json")
+
+
+class IndexPage(TemplateView):
+    template_name = "index.html"
